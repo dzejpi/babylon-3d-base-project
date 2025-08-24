8cc66c756dc4b29d80ed67f2b45c2b8c6c9362e6
DzejPi <dzejjpii@gmail.com>
2025-08-24 16:41:37 +0200

    Added splashConfig file with the basic splash screen configuration options.

---
+++ src/config/splashConfig.ts
@@ -0,0 +1,10 @@
+export const splashLogos = [
+    "assets/sprites/splash/logo-jam.png",
+    "assets/sprites/splash/logo-main.png",
+];
+
+export const splashSettings = {
+    fadeInDuration: 1000,
+    displayDuration: 1000,
+    fadeOutDuration: 1000,
+}
